bb10d9753cbc1483bb562d81767f8e63e00a76e5
totopizza <gren6158@yahoo.co.jp>
2017-03-21 02:31:53 +0900

    開始シーン明示化
    特に何もしてないし書くことないからそれっぽい事書いとけ

--- SquidDestiny/Main.cpp
+++ SquidDestiny/Main.cpp
@@ -10,12 +10,15 @@ void Main()
 	Window::SetTitle(L"イカのさだめ");
 	Window::Resize(375, 667);
 
+	// とりあえず置き
 	FontAsset::Register(L"ikamodoki", 35, L"イカモドキ");
 
 	MyGame manager;
 
 	manager.add<Title>(SceneName::Title);
 	manager.add<GameMain>(SceneName::GameMain);
+
+	manager.init(SceneName::Title);
 
 	while (System::Update())
 	{
